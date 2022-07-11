0cba52abe351a2dea4482147465c0bef8b4d9f94
PrajnaYaji18 <prajna@amagi.com>
2022-07-11 13:01:27 +0530

    update compare-version.rb

--- .circleci/compare_version.rb
+++ .circleci/compare_version.rb
@@ -1,6 +1,8 @@
-require_relative "../lib/sidekiq-dup-guard/version"
+require "rubygems"
 
-version = SidekiqDupGuard::VERSION
+spec = Gem::Specification::load("rubocop-amagi.gemspec")
+puts spec.version
+
 abort("Git Tag and SidekiqDupGuard version doesn't match") if (ENV["CIRCLE_TAG"].sub(/^v/, "") != version)
 
 exit(0)
