9545efcb03425bf26e23c0f2455518b5d5d5db75
PrajnaYaji18 <prajna@amagi.com>
2022-07-11 13:42:43 +0530

    convert version to string

--- .circleci/compare_version.rb
+++ .circleci/compare_version.rb
@@ -1,6 +1,6 @@
 require "rubygems"
 
-version = Gem::Specification::load("rubocop-amagi.gemspec").version
+version = Gem::Specification::load("rubocop-amagi.gemspec").version.to_s
 puts version
 
 abort("Git Tag and rubocop-amagi gem version doesn't match") if (ENV["CIRCLE_TAG"].sub(/^v/, "") != version)
