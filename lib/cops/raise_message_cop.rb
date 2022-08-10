eac35d5bc62c3bd2d4d9969f97019db0d990c8de
adip1818 <aditya.prasad@amagi.com>
2022-08-10 16:32:42 +0530

    Adding comments

--- lib/cops/raise_message_cop.rb
+++ lib/cops/raise_message_cop.rb
@@ -2,49 +2,70 @@
 
 # This cop checks if there is any "raise" code without a message/argument.
 # Examples:
-# Good -> raise "Message"
-# Good -> raise StandardError, 'message'
-# Good -> raise StandardError
+# Good 
+# `raise "Message"`
+# `raise StandardError, 'message'`
+# `raise StandardError`
 #
-# Bad -> raise
-# Bad -> raise unless status #inline conditions with raise
+# Bad 
+# `raise`
+# `raise unless status #inline conditions with raise`
 
 module RuboCop
   module Cop
     module CustomCops
       class RaiseMessageCop < Base
-
-        def_node_matcher :emptyraise?, <<~PATTERN
-        (send nil? :raise)
-        PATTERN
+        # We use #on_new_investigation instead of #initialize because #initialize is called for each class/const node
+        # whereas #on_new_investigation is called at the beginning of each file and we want to maintain
+        # these variables/arrays until the entire file is checked.
+        #
+        # @candidates  - Array to store all the possible empty nodes where an offense might occur.
+        # @rescnodes  - Array to store all the rescue nodes having an empty raise as a child.
+        # @finalnode  - Array to store the final nodes where offenses needed to be added.
         def on_new_investigation()
           super
-          @candidates=[] #candidates
-          @rescnodes=[] #rescuenodes
-          @safenodes=[] #evaluated
+          @candidates=[]
+          @rescnodes=[]
           @finalnodes=[]
         end
 
-        def on_rescue(node)
-          @rescnodes << node
+        # This pattern matcher is used to get all the occurences of empty raises.
+        def_node_matcher :empty_raise?, <<~PATTERN
+          (send nil? :raise)
+        PATTERN
+        # This pattern matcher is used to get all the rescue nodes having an empty raise descendant.
+        def_node_matcher :rescue_raises?, <<~PATTERN
+          (resbody ... `(send nil? :raise))
+        PATTERN
+
+        # This is used to pickup rescue nodes wrapping empty raises.
+        # The #rescue_raises? matcher will pickup only rescue nodes having empty raises and add them to @rescnodes
+        def on_resbody(node)
+          if rescue_raises?(node)
+            puts(node)
+            @rescnodes << node
+          end
         end
 
+        # This is used to pickup all the instances of empty raises.
+        # The #empty_raise? matcher will pickup all the empty raises and add them to @candidates.
         def on_send(node)
-          if emptyraise?(node)
+          if empty_raise?(node)
             @candidates << node
           end
         end
 
+        # We use #on_investigation_end here to add offenses after parsing the whole file.
         def on_investigation_end
-          @rescnodes.each do |rescnode|
-            @candidates.each do |node|
+          @candidates.each do |node|
+            flag = true
+            @rescnodes.each do |rescnode|
               if rescnode.source_range.contains?(node.source_range)
-                @safenodes << node
+                flag = false
+                break
               end
             end
-          end
-          @candidates.each do |n|
-            @finalnodes << n if (@safenodes.select{|s| s.object_id== n.object_id}).empty?
+            @finalnodes << node if flag
           end
           @finalnodes.each do |f|
             add_offense(f,message:"Raise should be accompanied by a message/argument.")
